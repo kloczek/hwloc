66fcd7e01d0348e78eaf281612d6aceb2866d742
Brice Goglin <Brice.Goglin@inria.fr>
2020-06-04 16:14:27 +0200

    Add a "levelzero" backend
    "Level Zero" is the lowlevel API for managing oneAPI devices.
    We create "ze%d" devices for these (mostly Intel) GPUs.
    "ze" is the prefix for function names in the levelzero API.
    
    CPPFLAGS should point to ze_api.h that is currently installed
    under $prefix/include/level_zero by the oneAPI compute runtime.
    
    This code was tested against the current 0.91 levelzero release,
    it doesn't build with the 0.95 spec but I was told to wait for
    1.0 later this year before updating.
    
    TODO add hwloc/levelzero.h helpers
    TODO add tests/hwloc/levelzero.c
    
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- include/hwloc/rename.h
+++ include/hwloc/rename.h
@@ -1,6 +1,6 @@
 /*
  * Copyright © 2009-2011 Cisco Systems, Inc.  All rights reserved.
- * Copyright © 2010-2019 Inria.  All rights reserved.
+ * Copyright © 2010-2020 Inria.  All rights reserved.
  * See COPYING in top-level directory.
  */
 
@@ -688,6 +688,7 @@ extern "C" {
 
 #define hwloc_cuda_component HWLOC_NAME(cuda_component)
 #define hwloc_gl_component HWLOC_NAME(gl_component)
+#define hwloc_levelzero_component HWLOC_NAME(levelzero_component)
 #define hwloc_nvml_component HWLOC_NAME(nvml_component)
 #define hwloc_rsmi_component HWLOC_NAME(rsmi_component)
 #define hwloc_opencl_component HWLOC_NAME(opencl_component)
